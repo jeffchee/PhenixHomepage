0f77342174d4c0838da72fa2354e5435c86cf042
jeffchee <jeffrey.chee@yahoo.com>
2020-04-09 09:34:29 -0700

    commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 var PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index1.html'));
 });
 
 
